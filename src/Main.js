0520d7eaeba703b4b926982ac89a460d4dc2d9f2
Dhruv Ramanujan <dhruv0122@gmail.com>
2018-06-12 14:21:03 -0400

    Style main

--- src/Main.js
+++ src/Main.js
@@ -6,7 +6,7 @@ import Chat from './Chat'
 class Main extends Component{
     render(){
         return(
-            <div className='Main'>
+            <div className='Main' style={styles}>
                 <Sidebar />
                 <Chat />
             </div>
@@ -14,5 +14,10 @@ class Main extends Component{
     }
 }
 
+const styles= {
+    display: 'flex',
+    alignItems: 'stretch',
+    height: '100vh',
+}
 
 export default Main
